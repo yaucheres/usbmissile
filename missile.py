c2461cdeb78b52c9736fc228bfb0b67c67552fe7
Yves Aucheres <yves.aucheres@gmail.com>
2015-06-05 19:13:35 +0200

    Update missile.py

--- missile.py
+++ missile.py
@@ -6,7 +6,7 @@ class NoMissilesError(Exception): pass
 
 class centerMissileDevice:
   dev       = None
-  handle    = None
+  ep        = None
   STOP      = 0x0
   LEFT      = 0x8
   RIGHT     = 0x4
@@ -18,47 +18,74 @@ class centerMissileDevice:
   RIGHTDOWN = RIGHT + DOWN
   FIRE      = 0x10
 
-  def __init__(self,usbdevice):
+  def __init__(self):
       try:
-        self.handle = usbdevice.open()
-        self.handle.reset()
-        return
+       self.dev = usb.core.find(idVendor=0x0416,idProduct=0x9391)
+       if self.dev is None:
+         raise ValueError('Device not found')
+       self.dev.set_configuration()
+       self.conf = self.dev.get_active_configuration()
+       self.intf = self.conf[(0,0)]
+       self.ep = usb.util.find_descriptor(
+        self.intf,
+        # match the first IN endpoint
+        custom_match = \
+        lambda e: \
+            usb.util.endpoint_direction(e.bEndpointAddress) == \
+            usb.util.ENDPOINT_IN)
+       return
       except NoMissilesError, e:
         raise NoMissilesError()
 
   def move(self, direction):
-     self.handle.controlMsg(0x21, 0x09, [0x5f, direction, 0xe0, 0xff, 0xfe], 0x0300, 0x00)
+     #self.ep.write(0x21, 0x09, [0x5f, direction, 0xe0, 0xff, 0xfe], 0x0300, 0x00)
+     #self.ep.write(0x21, [0x5f, direction, 0xe0, 0xff, 0xfe])
+     self.dev.ctrl_transfer(0x21, 0x09,0x0300,0x00,[0x5f, direction, 0xe0, 0xff, 0xfe])
 
-class UsbDevice:
-  def __init__(self):
-    self.handle = None
-    self.launcher = None
-    self.dev = None
-    self.busses = usb.busses()
 
-  def probe(self):
-    dev = usb.core.find(idVendor=0x0416,idProduct=0x9391)
-    self.dev = dev
-    self.conf = self.dev.configurations[0]
-    self.intf = self.conf.interfaces[0][0]
-    self.endpoints = []
-    for endpoint in self.intf.endpoints:
-      self.endpoints.append(endpoint)
-    self.launcher = legacyMissileDevice
-    return self.launcher
-
-    raise NoMissilesError()
-
-  def open(self):
-    if self.handle:
-      self.handle = None
-    self.handle = self.dev.open()
+class MissileNoDisplay:
+  def run(self):
     try:
-      self.handle.detachKernelDriver(0)
-      self.handle.detachKernelDriver(1)
-    except usb.USBError, err:
-      print >> sys.stderr, err
+        m = centerMissileDevice()
+    except NoMissilesError, e:
+        raise NoMissilesError
+    while 1:
+      keys = None
+      while not keys:
+        keys = raw_input("Enter something: ")
+      for k in keys:
+        if k == 'window resize':
+          size = self.ui.get_cols_rows()
+        elif k in ('w', 'up'):
+            m.move(m.UP)
+        elif k in ('x', 'down'):
+            m.move(m.DOWN)
+        elif k in ('a', 'left'):
+            m.move(m.LEFT)
+        elif k in ('d', 'right'):
+            m.move(m.RIGHT)
+        elif k in ('f', 'space'):
+            m.move(m.FIRE)
+        elif k in ('s'):
+            m.move(m.STOP)
+        elif k in ('q'):
+            m.move(m.LEFTUP)
+        elif k in ('e'):
+            m.move(m.RIGHTUP)
+        elif k in ('z'):
+            m.move(m.LEFTDOWN)
+        elif k in ('c'):
+            m.move(m.RIGHTDOWN)
+        elif k in ('r'):
+          for n in range(3):
+              sleep(0.5)
+        elif k in ('v'):
+            if  random.random() > 0.8:
+              m.move(m.FIRE)
+        elif k in ('esc'):
+          return
 
-    self.handle.setConfiguration(self.conf)
-    self.handle.claimInterface(self.intf)
-    self.handle.setAltInterface(self.intf)
+try:
+  MissileNoDisplay().run()
+except NoMissilesError, e:
+  print "No WMDs found."
